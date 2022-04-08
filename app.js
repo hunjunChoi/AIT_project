aa8ac119d490db80e6bedb4da476b6935097c603
Hyunjun Choi <guswns3723@gmail.com>
2022-04-08 13:51:29 -0400

    working on deployment

--- app.js
+++ app.js
@@ -1,32 +1,49 @@
+require("./db");
+require("./auth");
 
-require('./db');
-require('./auth');
+const passport = require("passport");
+const express = require("express");
+const path = require("path");
 
-const passport = require('passport');
-const express = require('express');
-const path = require('path');
-
-const routes = require('./routes/index');
-const list = require('./routes/list');
-const listItem = require('./routes/list-item');
+const routes = require("./routes/index");
+const list = require("./routes/list");
+const listItem = require("./routes/list-item");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
+
+const logger = (req, res, next) => {
+    console.log(req.method, req.path, req.query);
+    next();
+};
+app.use(logger);
+
+app.use((req, res, next) => {
+    if (req.get("Host")) {
+        next();
+    } else {
+        res.status(400).send("invalid request... add a host header plz");
+    }
+});
 
 // enable sessions
-const session = require('express-session');
+const session = require("express-session");
 const sessionOptions = {
-    secret: 'secret cookie thang (store this elsewhere!)',
+    secret: "secret cookie thang (store this elsewhere!)",
     resave: true,
-      saveUninitialized: true
+    saveUninitialized: true,
 };
 app.use(session(sessionOptions));
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+const staticPath = path.resolve(__dirname, "public");
+app.use(express.static(staticPath));
 
 // passport setup
 app.use(passport.initialize());
@@ -34,12 +51,14 @@ app.use(passport.session());
 
 // make user data available to all templates
 app.use((req, res, next) => {
-  res.locals.user = req.user;
-  next();
+    res.locals.user = req.user;
+    next();
 });
 
-app.use('/', routes);
-app.use('/list', list);
-app.use('/list-item', listItem);
+app.use("/", routes);
+app.use("/list", list);
+app.use("/list-item", listItem);
 
-app.listen(3000);
+app.listen(port, function () {
+    console.log(`Server running on port ${port}`);
+});
--- db.js
+++ db.js
@@ -1,34 +1,34 @@
-const mongoose = require('mongoose'),
-	URLSlugs = require('mongoose-url-slugs'),
-  passportLocalMongoose = require('passport-local-mongoose');
-
+const mongoose = require("mongoose"),
+    URLSlugs = require("mongoose-url-slugs"),
+    passportLocalMongoose = require("passport-local-mongoose");
 
 const User = new mongoose.Schema({
-  // username, password
-  lists:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'List' }]
+    // username, password
+    lists: [{ type: mongoose.Schema.Types.ObjectId, ref: "ootd" }],
 });
 
-const Item = new mongoose.Schema({
-	name: {type: String, required: true},
-	quantity: {type: Number, min: 1, required: true},
-	checked: {type: Boolean, default: false, required: true}
-}, {
-	_id: true
-});
-
+const Item = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        quantity: { type: Number, min: 1, required: true },
+        checked: { type: Boolean, default: false, required: true },
+    },
+    {
+        _id: true,
+    }
+);
 
 const List = new mongoose.Schema({
-  user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
-  name: {type: String, required: true},
-	createdAt: {type: Date, required: true},
-	items: [Item]
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    name: { type: String, required: true },
+    createdAt: { type: Date, required: true },
+    items: [Item],
 });
 
-
 User.plugin(passportLocalMongoose);
-List.plugin(URLSlugs('name'));
+List.plugin(URLSlugs("name"));
 
-mongoose.model('User', User);
-mongoose.model('List', List);
-mongoose.model('Item', Item);
-mongoose.connect('mongodb://localhost/grocerydb');
+mongoose.model("User", User);
+mongoose.model("List", List);
+mongoose.model("Item", Item);
+mongoose.connect("mongodb://localhost/closattire");
