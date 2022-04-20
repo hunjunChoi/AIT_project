88a9f9b5e60de90ebf79fe5771b772e606e0662a
Hyunjun Choi <guswns3723@gmail.com>
2022-04-19 20:52:19 -0400

    attempt to deploy on courant server

--- db.js
+++ db.js
@@ -31,4 +31,24 @@ List.plugin(URLSlugs("name"));
 mongoose.model("User", User);
 mongoose.model("List", List);
 mongoose.model("Item", Item);
-mongoose.connect("mongodb://localhost/closattire");
+
+// is the environment variable, NODE_ENV, set to PRODUCTION?
+let dbconf;
+if (process.env.NODE_ENV === "PRODUCTION") {
+    // if we're in PRODUCTION mode, then read the configration from a file
+    // use blocking file io to do this...
+    const fs = require("fs");
+    const path = require("path");
+    const fn = path.join(__dirname, "config.json");
+    const data = fs.readFileSync(fn);
+
+    // our configuration file will be in json, so parse it and set the
+    // conenction string appropriately!
+    const conf = JSON.parse(data);
+    dbconf = conf.dbconf;
+} else {
+    // if we're not in PRODUCTION mode, then use
+    dbconf = "mongodb://localhost/closattire";
+}
+
+mongoose.connect(dbconf);
