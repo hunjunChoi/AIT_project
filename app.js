7de5508c8f29944712c0b6711484a726cde5230f
Hyunjun Choi <guswns3723@gmail.com>
2022-07-12 15:16:27 -0400

    testing

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const closet = require("./routes/closet");
 const listItem = require("./routes/list-item");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1392;
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
